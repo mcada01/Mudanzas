3ee3cf523797f50c881473280eb53c65ee77ff50
Melissa Cadavid Peláez <mcada01@hotmail.com>
2019-05-18 14:56:46 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppService } from './app.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,10 @@ export class AppComponent  implements OnInit{
   registerForm: FormGroup;
   submitted = false;
   file: File;
-  fileByteArray = [];
+  numDocumento: number = null;
+  fileUrl;
 
-  constructor(private formBuilder: FormBuilder, private appService: AppService) { }
+  constructor(private formBuilder: FormBuilder, private appService: AppService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     // inicializar los campos del formulario con sus respectivas validaciones
@@ -22,10 +24,26 @@ export class AppComponent  implements OnInit{
       numDocumento: ['', Validators.required],
       archivo: ['', Validators.required]
     });
+
+    // construir archivo de texto
+    const data = this.file;
+    const blob = new Blob([data], { type: 'text/plain' });
+
+    //this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
   }
 
   // para acceder facil a los campos
   get f() { return this.registerForm.controls; }
+
+  downLoadFile(data: any, type: string) {
+    var blob = new Blob([data], { type: type});
+    var url = window.URL.createObjectURL(blob);
+    var pwa = window.open(url);
+    if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+        alert( 'Please disable your Pop-up blocker and try again.');
+    }
+  }
 
   procesarArchivo() {
       this.submitted = true;
@@ -35,12 +53,28 @@ export class AppComponent  implements OnInit{
           return;
       }
 
-      this.appService.procesarArchivo(this.file).subscribe(Response => {
-        debugger;
-        if (Response.exitoso)
-          alert('SUCCESS!!');
+    
+
+      this.appService.procesarArchivo(this.file, this.numDocumento).subscribe(Response => {
+        this.downLoadFile(Response,"text/plain")
+        console.log(Response);
+        
+        alert('SUCCESS!!');
+
+          /*let filename = "C:\ArchivoSalida.txt";//"/Path/to/your/report.pdf";
+          this.appService.downloadReport(filename).subscribe(
+            data => {
+              //saveAs(data, filename);
+            },
+            err => {
+              alert("Problem while downloading the file.");
+              console.error(err);
+            }
+          );
+          
+        }
         else
-        alert('ERROR!!');
+          alert('ERROR!!');*/
       });
 
       // leer el archivo 
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -7,10 +7,28 @@ export class AppService {
     constructor(private http: HttpClient) {
     }
 
-    procesarArchivo(file: File): Observable<any> {
+    procesarArchivo(file: File, numDocumento: number){
         let formData = new FormData();
         formData.append('ArchivoSeleccionado', file);
-        return this.http.post('http://localhost/ProcesarArchivoMudanzas/api/Mudanza/PostProcesarArchivo', formData);//return this.http.post('http://localhost/ProcesarArchivoMudanzas/api/Mudanza/PostProcesarArchivo',fd);
-    }
+        //return this.http.post('http://localhost/ProcesarArchivoMudanzas/api/Mudanza/PostProcesarArchivo/' + numDocumento, formData)
+        return this.http.post('http://localhost:56550/api/Mudanza/PostProcesarArchivo/' + numDocumento, formData
+        , {
+           //headers: new HttpHeaders().append("Accept", "text/plain"),
+           responseType : "arraybuffer"
+          }
+          )
+        
+        }
+
+    public downloadReport(file): Observable<any> {
+        // Create url
+        let url = 'http://localhost/ProcesarArchivoMudanzas/api/Mudanza/PostDescargarArchivo/';
+        var body = { filename: file };
+    
+        return this.http.post(url, body, {
+          responseType: "blob",
+          headers: new HttpHeaders().append("Content-Type", "application/json")
+        });
+      }
 
 }
