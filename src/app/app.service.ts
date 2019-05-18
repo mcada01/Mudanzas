77f3de8e30b6721020cf9b691c9c4c1bc8b14c5b
Melissa Cadavid Peláez <mcada01@hotmail.com>
2019-05-18 15:48:13 -0500

    descargar archivo

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AppService {
@@ -10,25 +10,12 @@ export class AppService {
     procesarArchivo(file: File, numDocumento: number){
         let formData = new FormData();
         formData.append('ArchivoSeleccionado', file);
-        //return this.http.post('http://localhost/ProcesarArchivoMudanzas/api/Mudanza/PostProcesarArchivo/' + numDocumento, formData)
-        return this.http.post('http://localhost:56550/api/Mudanza/PostProcesarArchivo/' + numDocumento, formData
+        return this.http.post(environment.apiUrl + numDocumento, formData
         , {
-           //headers: new HttpHeaders().append("Accept", "text/plain"),
            responseType : "arraybuffer"
           }
           )
         
         }
-
-    public downloadReport(file): Observable<any> {
-        // Create url
-        let url = 'http://localhost/ProcesarArchivoMudanzas/api/Mudanza/PostDescargarArchivo/';
-        var body = { filename: file };
-    
-        return this.http.post(url, body, {
-          responseType: "blob",
-          headers: new HttpHeaders().append("Content-Type", "application/json")
-        });
-      }
 
 }
